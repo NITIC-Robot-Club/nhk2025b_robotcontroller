b8ef955fb77de91a4d3d8b7343a165c56fa5b3c6
VeroUfomia1398 <st22188sk@gm.ibaraki-ct.ac.jp>
2025-05-16 21:28:00 +0900

    [add]visualize result

--- Assets/CSscript/PanelContoroller.cs
+++ Assets/CSscript/PanelContoroller.cs
@@ -9,11 +9,16 @@ public class PanelContoroller : MonoBehaviour
     [SerializeField] GameObject conPanel;
     [SerializeField] GameObject mainPanel;
     [SerializeField] GameObject menuPanel;
+    [SerializeField] GameObject infoPanel;
     [SerializeField] Button toCon;
     //[SerializeField] Button toMain;
-    [SerializeField] private SpriteSwitcher spriteSwitcher;
+    [SerializeField] private SpriteSwitcher conSwitcher;
+    [SerializeField] private SpriteSwitcher infoSwitcher;
     private bool is_mainpanel = true;
     public GameObject JoyCpn;
+    private bool previousMain = true;
+    private bool previousCon = false;
+    private bool previousInfo = false;
  
     void Start () {
         toCon.onClick.AddListener(conOn);
@@ -38,18 +43,94 @@ public class PanelContoroller : MonoBehaviour
     }
     void UpdatePanelState()
     {
-        if (spriteSwitcher.GetisOn())
+        if (conSwitcher.GetisOn())
         {
-            is_mainpanel = false;
-            conPanel.SetActive(true);
-            mainPanel.SetActive(false);
+            if (infoSwitcher.GetisOn())
+            {
+                is_mainpanel = false;
+                previousMain = false;
+                previousCon = false;
+                previousInfo = true;
+                infoPanel.SetActive(false);
+                conPanel.SetActive(true);
+            }
+            else 
+            {
+                is_mainpanel = false;
+                previousMain = false;
+                previousCon = true;
+                previousInfo = false;
+                conPanel.SetActive(true);
+                mainPanel.SetActive(false);
+            }
         }
         else
         {
-            is_mainpanel = true;
+            if (infoSwitcher.GetisOn())
+            {
+                is_mainpanel = false;
+                previousMain = false;
+                previousCon = false;
+                previousInfo = true;
+                conPanel.SetActive(false);
+                infoPanel.SetActive(true);
+            }
+            else
+            {
+                is_mainpanel = true;
+                previousMain = true;
+                previousCon = false;
+                previousInfo = false;
+                conPanel.SetActive(false);
+                mainPanel.SetActive(true);
+            }
+        }
+        if (infoSwitcher.GetisOn())
+        {
+            if (is_mainpanel)
+            {
+                previousMain = true;
+                previousCon = false;
+                previousInfo = true;
+            }
+            else
+            {
+                previousMain = false;
+                previousCon = true;
+                previousInfo = false;
+            }
+            is_mainpanel = false;
             conPanel.SetActive(false);
-            mainPanel.SetActive(true);
+            mainPanel.SetActive(false);
+            infoPanel.SetActive(true);
             JoyCpn.GetComponent<UnityPublisher>().ResetJoystickInput();
+        }
+        else
+        {
+            if (previousMain)
+            {
+                is_mainpanel = true;
+                conPanel.SetActive(false);
+                mainPanel.SetActive(true);
+                infoPanel.SetActive(false);
+                JoyCpn.GetComponent<UnityPublisher>().ResetJoystickInput();
+            }
+            else if (previousCon)
+            {
+                is_mainpanel = false;
+                conPanel.SetActive(true);
+                mainPanel.SetActive(false);
+                infoPanel.SetActive(false);
+                JoyCpn.GetComponent<UnityPublisher>().ResetJoystickInput();
+            }
+            else if (previousInfo)
+            {
+                is_mainpanel = false;
+                conPanel.SetActive(false);
+                mainPanel.SetActive(false);
+                infoPanel.SetActive(true);
+                JoyCpn.GetComponent<UnityPublisher>().ResetJoystickInput();
+            }
         }
     }
     public bool Getismain(){
--- Assets/CSscript/UnitySubscriber.cs
+++ Assets/CSscript/UnitySubscriber.cs
@@ -23,7 +23,7 @@ public class UnitySubscriber : MonoBehaviour
     private ISubscription<Ps> currentpose_sub;
     private ISubscription<Ps> goalpose_sub;
     private ISubscription<Pa> path_sub;
-    private ISubscription<Ps> lookahreadpose_sub;
+    private ISubscription<Ps> lookaheadpose_sub;
     private ISubscription<Sw> result_sub;
     private ISubscription<Sw> cmd_sub;
 
@@ -37,6 +37,7 @@ public class UnitySubscriber : MonoBehaviour
     const float m2pixY = 813.47f / 5.0f;            // px/m
     const float anchorX = -100f;                    // px
     const float anchorY = -100f;                    // px
+
     //Current Pose Subscriber
     public GameObject robot;
     private RectTransform robotRectTransform;
@@ -53,6 +54,28 @@ public class UnitySubscriber : MonoBehaviour
     private float goriZ;
     private float goriW;
 
+    //Lookahead Subscriber
+    public GameObject lookahead;
+    private RectTransform lookaheadRectTransform;
+    private float lposX;
+    private float lposY;
+    private float loriZ;
+    private float loriW;
+
+    //Result Subscriber
+    const float maxSpeed = 1000.0f;
+    public Color minColor = Color.green;
+    public Color maxColor = Color.red;
+    private Renderer rend;
+    [SerializeField] private TMP_Text swerveText0;
+    [SerializeField] private TMP_Text swerveText1;
+    [SerializeField] private TMP_Text swerveText2;
+    [SerializeField] private TMP_Text swerveText3;
+    public GameObject[] swerve = new GameObject[4];
+    private RectTransform[] swerveRectTransform = new RectTransform[4];
+    private float[] wheelAngle = new float[4];
+    private float[] wheelSpeed = new float[4];
+    private float angleDeg = 0f;
 
     void Start()
     {
@@ -61,6 +84,13 @@ public class UnitySubscriber : MonoBehaviour
         robotRectTransform.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
         goalRectTransform = (RectTransform)goal.transform;
         goalRectTransform.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+        lookaheadRectTransform = (RectTransform)lookahead.transform;
+        lookaheadRectTransform.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+
+        for (int i = 0; i < 4; i++)
+        {
+            swerveRectTransform[i] = (RectTransform)swerve[i].transform;
+        }
     }
 
     void Update()
@@ -74,17 +104,33 @@ public class UnitySubscriber : MonoBehaviour
                 currentpose_sub = ros2Node.CreateSubscription<Ps>("/localization/current_pose", currentposeCallback);
                 goalpose_sub = ros2Node.CreateSubscription<Ps>("/behavior/goal_pose", goalposeCallback);
                 path_sub = ros2Node.CreateSubscription<Pa>("/planning/path", pathCallback);
-                lookahreadpose_sub = ros2Node.CreateSubscription<Ps>("/control/lookahread_pose", lookahreadposeCallback);
-                result_sub = ros2Node.CreateSubscription<Sw>("/visualization/swerve/result", resultCallback);
-                cmd_sub = ros2Node.CreateSubscription<Sw>("/visualization/swerve/cmd", cmdCallback);
+                lookaheadpose_sub = ros2Node.CreateSubscription<Ps>("/control/lookahead_pose", lookaheadposeCallback);
+                result_sub = ros2Node.CreateSubscription<Sw>("/swerve/result", resultCallback);
+                cmd_sub = ros2Node.CreateSubscription<Sw>("/visualization/swerve", cmdCallback);
             }
         }
-        mapTopicText.SetText(data);
+        if (mapTopicText != null) mapTopicText.SetText(data);
 
+        lookaheadRectTransform.anchoredPosition = new Vector3(lposX, lposY, 0f);
+        lookahead.transform.rotation = Quaternion.Euler(0f, 0f, 90f) * new Quaternion(0f, 0f, loriZ, loriW);
         goalRectTransform.anchoredPosition = new Vector3(gposX, gposY, 0f);
         goal.transform.rotation = Quaternion.Euler(0f, 0f, 90f) * new Quaternion(0f, 0f, goriZ, goriW);
         robotRectTransform.anchoredPosition = new Vector3(posX, posY, 0f);
         robot.transform.rotation = Quaternion.Euler(0f, 0f, 90f) * new Quaternion(0f, 0f, oriZ, oriW);
+
+        if (swerveText0 != null) swerveText0.SetText($"WheelAngle0: {wheelAngle[0]}°\nWheelSpeed0: {wheelSpeed[0]}rpm");
+        if (swerveText1 != null) swerveText1.SetText($"WheelAngle1: {wheelAngle[1]}°\nWheelSpeed1: {wheelSpeed[1]}rpm");
+        if (swerveText2 != null) swerveText2.SetText($"WheelAngle2: {wheelAngle[2]}°\nWheelSpeed2: {wheelSpeed[2]}rpm");
+        if (swerveText3 != null) swerveText3.SetText($"WheelAngle3: {wheelAngle[3]}°\nWheelSpeed3: {wheelSpeed[3]}rpm");
+
+        for (int i = 0; i < swerve.Length; i++)
+        {
+            float t = Mathf.Clamp01(Mathf.Abs(wheelSpeed[i]) / maxSpeed);
+            Color color = Color.Lerp(minColor, maxColor, t);
+            var image = swerve[i]?.GetComponent<UnityEngine.UI.Image>();
+            image.color = color;
+            swerveRectTransform[i].transform.rotation = Quaternion.Euler(0f, 0f, wheelAngle[i] + 180f);
+        }
     }
 
     void mappingCallback(Og msg)
@@ -115,12 +161,33 @@ public class UnitySubscriber : MonoBehaviour
     {
     }
 
-    void lookahreadposeCallback(Ps msg)
+    void lookaheadposeCallback(Ps msg)
     {
+        lposX = -(float)msg.Pose.Position.X * m2pixY + anchorX;
+        lposY = -(float)msg.Pose.Position.Y * m2pixX + anchorY;
+        loriZ = -(float)msg.Pose.Orientation.Z;
+        loriW = -(float)msg.Pose.Orientation.W;
     }
 
     void resultCallback(Sw msg)
     {
+        for (int i = 0; i < 4; i++)
+        {
+            if (-1.0f <= msg.Wheel_speed[i] && msg.Wheel_speed[i] <= 1.0f)
+            {
+                wheelSpeed[i] = 0f;
+            }
+            else if (msg.Wheel_speed[i] < -1.0f)
+            {
+                wheelSpeed[i] = Mathf.Abs(msg.Wheel_speed[i]);
+            }
+            else 
+            {
+                wheelSpeed[i] = (float)msg.Wheel_speed[i];
+            }
+            wheelAngle[i] = (float)msg.Wheel_angle[i] * Mathf.Rad2Deg;
+            if (wheelAngle[i] > 180f) wheelAngle[i] -= 360f;
+        }
     }
 
     void cmdCallback(Sw msg)
